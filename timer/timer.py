d6d34d90129baab021300a90e323948275a93a75
Coleman McFarland <coleman.mcfarland@gmail.com>
2013-04-19 22:38:50 -0400

    pushing timer script

--- timer/timer.py
+++ timer/timer.py
@@ -12,16 +12,31 @@ start_time = time.time()
 def main(args):
     parser = argparse.ArgumentParser(description='timer from the command line', prog='timer')
     parser.add_argument('minutes', type=int)
-    opts = parser.parse_args(args) # comman line arguments
-    startTimer(opts.minutes)
+    parser.add_argument('message', nargs='*', type=str)
+    opts = parser.parse_args(args) # command line arguments
+    startTimer(opts.minutes, opts.message)
 
-def startTimer(minutes):
-    message = 'Starting %s minute timer.' % minutes
-    os.system('notify-send \'Starting timer\'')
+def startTimer(minutes, message):
+
+    message = ' '.join(message)
+    os.system('notify-send \' '+ message +'\'')
     seconds = minutes * 60
+    os.system('notify-send \'Starting ' + str(minutes) + ' minute timer\'')
     time.sleep(seconds)
-    os.system('notify-send \'Timer is up\'')
+    os.system('notify-send \'Timer is up\n '+ message +'\'')
+
+
+def displayMessage(message):
+    pass
+
+#    message = ' '.join(message)
+#    os.system('notify-send \' '+ message +'\'')
+#    seconds = minutes * 60
+#    os.system('notify-send \'Starting ' + str(minutes) + ' minute timer\'')
+#    time.sleep(seconds)
+#    os.system('notify-send \'Timer is up\n '+ message +'\'')
 
 
 if __name__ == '__main__':    
     main(sys.argv[1:])
+
