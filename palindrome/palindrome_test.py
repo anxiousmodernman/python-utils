9c6d8b484d6504bddb1368adfaaf584cdbe88497
Coleman McFarland <coleman.mcfarland@gmail.com>
2013-04-26 13:16:53 -0400

    palindrome algorithm started

---
+++ palindrome/palindrome_test.py
@@ -0,0 +1,28 @@
+import unittest
+from palindrome import *
+
+
+class PalindromeTest(unittest.TestCase):
+
+    def testListMakerNone(self):
+        pass
+
+
+    def testListMakerEven(self):
+        pass
+
+
+    def testListMakerOdd(self):
+        pass
+
+
+
+
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
+        
